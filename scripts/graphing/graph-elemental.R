43c2e533fce80435465ced6b38d90175c0e18908
Andrey Koval <koval.andrey@gmail.com>
2017-05-03 17:57:58 -0400

    added function to count measure over time

--- scripts/graphing/graph-elemental.R
+++ scripts/graphing/graph-elemental.R
@@ -2,7 +2,53 @@
 #########################################################
 ### Declare functions to build elemental graphs
 #########################################################
+# plots value counts of a measure over a metric of time
+count_over_time <- function(
+  d              # dataset
+  ,time_name     # goes on x
+  ,measure_name  # mapped to fill
+  ,color_palette = "auto" # custom colors for fill
+){
+  # use these assignments for testing and development
+  # d  <- ds
+  # time_name     = "year"
+  # measure_name  = "male"
+  # color_palette = color_male
+  # create dataset to be used for graphing
+  d1 <- d %>% 
+    dplyr::group_by_(time_name, measure_name) %>% 
+    dplyr::summarize(n=n()) %>% 
+    dplyr::group_by_(time_name) %>% 
+    dplyr::mutate(
+      time_sum = sum(n)
+    ) %>% 
+    dplyr::ungroup() %>% 
+    dplyr::mutate(
+      pct = scales::percent( n / time_sum )
+    )
+  # create the graph
+  g <- d1 %>% 
+    ggplot(aes_string(x=time_name, y="n", fill = measure_name)) +
+    geom_bar(stat = "identity")+
+    geom_text( inherit.aes = TRUE,
+               aes_string(x = time_name, y = "n", label = "pct") 
+               # ,nudge_y = 1000 # see recipe
+               # ,colour   = "white"
+               # ,position = position_dodge(.9)
+               # ,size=3
+    )+
+    theme_minimal()
+  if(!color_palette=="auto"){
+    g <- g + scale_fill_manual(values=color_palette)
+  }
+  return(g)
+}
+# usage:
+# ds %>% count_over_time(time_name = "year", measure_name = "male",color_palette = color_male)
+# ds %>% count_over_time("year","race",color_race)
 
+
+# builds a spaghetti plot from raw data
 elemental_line <- function(
   d_observed,
   variable_name,
