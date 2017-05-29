dcfb567d00f4142eb0915ade0879c85e84905cb8
Cassandra <clb@uvic.ca>
2017-05-29 10:33:30 -0700

    Scripts testing package for APA style paper writing

---
+++ sandbox/Test.R
@@ -0,0 +1,9 @@
+# knitr::stitch_rmd(script="./___/___.R", output="./___/___/___.md")
+#These first few lines run only when the file is run in RStudio, !!NOT when an Rmd/Rnw file calls it!!
+rm(list=ls(all=TRUE))  #Clear the variables from previous runs.
+cat("\f") # clear console 
+
+# ---- load-packages -----------------------------------------------------------
+# Attach these packages so their functions don't need to be qualified: http://r-pkgs.had.co.nz/namespace.html#search-path
+library(magrittr) # enables piping : %>% 
+library(dplyr)
